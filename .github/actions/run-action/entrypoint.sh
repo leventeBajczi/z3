aeb3002382c3106d3d08a8013f5d32d54ce1a0b2
Levente Bajczi <bajczilevi@gmail.com>
2024-03-10 21:57:17 +0100

    Updated linux release

--- .github/actions/run-action/entrypoint.sh
+++ .github/actions/run-action/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-RUN mkdir release
-RUN python2.7 scripts/mk_make.py --prefix=$PWD/release --java
-RUN cd build/ && make -j $(nproc)
+mkdir release
+python2.7 scripts/mk_make.py --prefix=$PWD/release --java
+cd build/ && make -j $(nproc)
